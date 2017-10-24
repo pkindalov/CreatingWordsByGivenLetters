607f3333d07a9b4dc5c79d3607be520dc309f714
pkindalov <panayotkindalov@gmail.com>
2017-10-24 12:36:36 +0300

    Little change in algorithm. Added while loop and new method for generating words

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,44 @@ public class Main {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Enter string letters: ");
-        String userInput = reader.readLine();
 
-        char[] letters = userInput.toCharArray();
 
+        System.out.println("Enter count of letters of word to generate(2-6):");
+        System.out.println("For exit just enter exit or stop");
+        String command = reader.readLine();
+
+        while (!command.equals("stop") && !command.equals("exit")){
+
+            int lettersCount = Integer.parseInt(command);
+
+            System.out.println("Enter string letters: ");
+            String userInput = reader.readLine();
+
+            char[] letters = userInput.toCharArray();
+
+            switch (lettersCount){
+                case 5:
+                    gen5LettersWords(letters);
+                    break;
+                default:
+                    System.out.println("Sorry, too big or small value you entered. You can use numbers from two to six");
+                    break;
+            }
+
+
+            System.out.println("Enter count of letters of word to generate(2-6):");
+            System.out.println("For exit just enter exit or stop");
+            command = reader.readLine();
+        }
+
+
+
+
+
+
+    }
+
+    private static void gen5LettersWords(char[] letters) {
         for(int pos1 = 0; pos1 < letters.length; pos1++){
             for(int pos2 = 0; pos2 < letters.length; pos2++){
                for(int pos3 = 0; pos3 < letters.length; pos3++){
@@ -62,128 +95,8 @@ public class Main {
         }
 
 
-//        String word = "";
-//        char[] letters = new char[] {'с' , 'т', 'м', 'р', 'а', 'о', 'й'};
-
-
-//        for(int i = 0; i < letters.length - 1; i++){
-//            letters[i] = letters[i + 1];
-//            letters[i + 1] = letters[i];
-////
-//            change3letters(letters);
-//            change3letters(letters);
-
-
-//            changeLettersPosToCreateWord(letters);
-//
-//            makingWordsWithFirst3Letters(letters);
-//
-//            makingWordWithLast3letters(letters);
-
 //        }
-
-
-
-
     }
 
-    private static void change3letters(char[] letters) {
-        //all 3 combinations first part of array
-        StringBuilder word = new StringBuilder();
-        int index = letters.length / 2;
-
-
-        changePositionOfLetters(letters, word, index);
-
-        changeLetterPosFromMiddleToBottom(letters, word, index);
-
-
-        for(int a = 0; a < letters.length; a++){
-            word.append(letters[a]);
-            for(int b = a + 1; b < letters.length; b++){
-                word.append(letters[b]);
-            }
-            System.out.println(word.toString());
-            word.delete(0, word.length());
-        }
-
-
-
-    }
-
-    private static void changeLetterPosFromMiddleToBottom(char[] letters, StringBuilder word, int index) {
-        for(int a = index; a < letters.length; a++){
-            word.append(letters[a]);
-            for(int b = index + 1; b < letters.length; b++){
-                word.append(letters[b]);
-            }
-            System.out.println(word.toString());
-            word.delete(0, word.length());
-
-        }
-    }
-
-    private static void changePositionOfLetters(char[] letters, StringBuilder word, int index) {
-        for(int a = 0; a < index; a++){
-            word.append(letters[a]);
-            for(int b = a + 1; b <= index; b++){
-
-                word.append(letters[b]);
-            }
-            System.out.println(word.toString());
-            word.delete(0, word.length());
-
-        }
-        System.out.println(word.toString());
-
-    }
-
-//    private static void makingWordWithLast3letters(char[] letters) {
-//        StringBuilder wordBuilder = new StringBuilder();
-//        for(int l = (letters.length / 2); l >= 0; l--){
-////            System.out.print(letters[l]);
-//            wordBuilder.append(letters[l]);
-//            System.out.println(wordBuilder.toString());
-//            for(int l2 = (letters.length / 2) - 1; l2 >= 0; l2--){
-//                  wordBuilder.append(letters[l2]);
-//                  System.out.println(wordBuilder.toString());
-////                System.out.print(letters[l2]);
-//            }
-//            System.out.println();
-//        }
-////        System.out.println(wordBuilder.toString());
-//    }
-//
-//    private static void makingWordsWithFirst3Letters(char[] letters) {
-//        StringBuilder wordBuilder = new StringBuilder();
-//        for(int l = 0; l < letters.length / 2; l++){
-//             wordBuilder.append(letters[l]);
-////             System.out.println(wordBuilder.toString());
-//            System.out.print(letters[l]);
-//            for(int l2 = 1; l2 < letters.length / 2; l2++){
-//                  wordBuilder.append(letters[l2]);
-////                  System.out.println(wordBuilder.toString());
-//                System.out.print(letters[l2]);
-//            }
-//            System.out.println();
-//        }
-////        System.out.println(wordBuilder.toString());
-//    }
-//
-//    private static void changeLettersPosToCreateWord(char[] letters) {
-//        StringBuilder wordBuilder = new StringBuilder();
-//        for(int l = 0; l < letters.length; l++){
-////            wordBuilder.append(letters[l]);
-//            System.out.print(letters[l]);
-//            for(int l2 = 1; l2 < letters.length; l2++){
-////                wordBuilder.append(letters[l2]);
-//                System.out.print(letters[l2]);
-//
-//            }
-//            System.out.println();
-//        }
-////        System.out.println(wordBuilder.toString());
-//
-//    }
 
 }
