ce6306d8a63cf66e8bc762b4be941164d27764f3
pkindalov <panayotkindalov@gmail.com>
2017-10-25 18:32:48 +0300

    Finished methods and added more words to dictionary file

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,19 @@ public class Main {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder generatedWord = new StringBuilder();
+        StringBuilder letterCombinationWord = new StringBuilder();
         Reader dictReader = new Reader();
 
-        generatedWord.append(dictReader.readFile());
-        System.out.println(generatedWord.toString());
+        generatedWord.append(dictReader.readFile().toLowerCase());
+        String[] allAvailableWordsBG = generatedWord.toString().split(" ");
+//        System.out.println(generatedWord.toString());
 
-
+        System.out.println("Available words in dictionary: " + allAvailableWordsBG.length);
         System.out.println("Enter count of letters of word to generate(2-6):");
-        System.out.println("For exit just enter exit or stop");
+        System.out.println("(For exit just enter exit or stop)");
         String command = reader.readLine();
 
-        while (!command.equals("stop") && !command.equals("exit")){
+        while (!command.equals("stop") && !command.equals("exit") && !command.equals("еьит")){
 
             int lettersCount = Integer.parseInt(command);
 
@@ -32,22 +34,22 @@ public class Main {
 
             switch (lettersCount){
                 case 2:
-                    gen2LettersWords(letters);
+                    gen2LettersWords(letters, letterCombinationWord, allAvailableWordsBG, 2);
                     break;
                 case 3:
-                    gen3LettersWords(letters);
+                    gen3LettersWords(letters, letterCombinationWord, allAvailableWordsBG, 3);
                     break;
                 case 4:
-                    gen4LettersWords(letters);
+                    gen4LettersWords(letters, letterCombinationWord, allAvailableWordsBG, 4);
                     break;
                 case 5:
-                    gen5LettersWords(letters);
+                    gen5LettersWords(letters, letterCombinationWord, allAvailableWordsBG, 5);
                     break;
                 case 6:
-                    gen6LettersWords(letters);
+                    gen6LettersWords(letters, letterCombinationWord, allAvailableWordsBG, 6);
                     break;
                 case 7:
-                    gen7LettersWords(letters);
+                    gen7LettersWords(letters, letterCombinationWord, allAvailableWordsBG, 7);
                     break;
                 default:
                     System.out.println("Sorry, too big or small value you entered. You can use numbers from two to six");
@@ -67,57 +69,108 @@ public class Main {
 
     }
 
-    private static void gen2LettersWords(char[] letters){
+    private static void searchForAword(String str, StringBuilder letterCombinationWord, String[] allAvailableWordsBG, int size) {
+        letterCombinationWord.append(str);
+//
+        for(int i = 0; i < allAvailableWordsBG.length; i++){
+            if(allAvailableWordsBG[i].contains(letterCombinationWord.toString()) && allAvailableWordsBG[i].length() == size){
+                System.out.println(allAvailableWordsBG[i]);
+            }
+        }
+    }
+
+    private static void gen2LettersWords(char[] letters, StringBuilder letterCombinationWord, String[] allAvailableWordsBG, int size){
 
         for(int pos1 = 0; pos1 < letters.length; pos1++){
             for(int pos2 = 0; pos2 < letters.length; pos2++){
-                for(int pos3 = 0; pos3 < letters.length; pos3++){
-                    for(int pos4 = 0; pos4 < letters.length; pos4++){
-                        for(int pos5 = 0; pos5 < letters.length; pos5++){
-                            for (int pos6 = 0; pos6 < letters.length; pos6++){
-                                if(pos1 != pos2){
-                                    if(pos2 != pos3){
-                                        if(pos3 != pos4){
-                                            if(pos4 != pos5){
-                                                if(pos5 != pos6){
-
-                                                    System.out.print("" + letters[pos1] + letters[pos2] + " ");
-                                                    System.out.print("" + letters[pos2] + letters[pos3] + " ");
-                                                    System.out.print("" + letters[pos3] + letters[pos4] + " ");
-                                                    System.out.print("" + letters[pos5] + letters[pos6] + " ");
-                                                    System.out.print("" + letters[pos4] + letters[pos5] + " ");
-                                                    System.out.println();
-                                                }
-//                                                System.out.println();
-                                            }
-                                        }
-                                    }
-                                }
-
-                            }
-                        }
-                    }
-
+                if(pos1 != pos2){
+                    searchForAword("" + letters[pos1] + letters[pos2], letterCombinationWord, allAvailableWordsBG, size);
+                    letterCombinationWord.setLength(0);
                 }
             }
         }
 
+//        for(int pos1 = 0; pos1 < letters.length; pos1++){
+//            for(int pos2 = 0; pos2 < letters.length; pos2++){
+//                for(int pos3 = 0; pos3 < letters.length; pos3++){
+//                    for(int pos4 = 0; pos4 < letters.length; pos4++){
+//                        for(int pos5 = 0; pos5 < letters.length; pos5++){
+//                            for (int pos6 = 0; pos6 < letters.length; pos6++){
+//                                if(pos1 != pos2){
+//                                    if(pos2 != pos3){
+//                                        if(pos3 != pos4){
+//                                            if(pos4 != pos5){
+//                                                if(pos5 != pos6){
+//
+//                                                    searchForAword("" + letters[pos1] + letters[pos2], letterCombinationWord, allAvailableWordsBG);
+//                                                       letterCombinationWord.setLength(0);
+////
+////                                                    searchForAword("" + letters[pos2] + letters[pos3], letterCombinationWord, allAvailableWordsBG);
+////                                                    letterCombinationWord.setLength(0);
+////
+////                                                    searchForAword("" + letters[pos3] + letters[pos4], letterCombinationWord, allAvailableWordsBG);
+////                                                    letterCombinationWord.setLength(0);
+////
+////                                                    searchForAword("" + letters[pos5] + letters[pos6], letterCombinationWord, allAvailableWordsBG);
+////                                                    letterCombinationWord.setLength(0);
+////
+////                                                    searchForAword("" + letters[pos4] + letters[pos5], letterCombinationWord, allAvailableWordsBG);
+////                                                    letterCombinationWord.setLength(0);
+//
+////                                                    System.out.print("" + letters[pos1] + letters[pos2] + " ");
+////                                                    System.out.print("" + letters[pos2] + letters[pos3] + " ");
+////                                                    System.out.print("" + letters[pos3] + letters[pos4] + " ");
+////                                                    System.out.print("" + letters[pos5] + letters[pos6] + " ");
+////                                                    System.out.print("" + letters[pos4] + letters[pos5] + " ");
+////                                                    System.out.println();
+//                                                }
+////                                                System.out.println();
+//                                            }
+//                                        }
+//                                    }
+//                                }
+//
+//                            }
+//                        }
+//                    }
+//
+//                }
+//            }
+//        }
+
 
     }
 
-    private static void gen3LettersWords(char[] letters) {
+    private static void gen3LettersWords(char[] letters, StringBuilder letterCombinationWord, String[] allAvailableWordsBG, int size) {
 
         for(int pos1 = 0; pos1 < letters.length; pos1++){
             for(int pos2 = 0; pos2 < letters.length; pos2++){
                 for(int pos3 = 0; pos3 < letters.length; pos3++){
                     if(pos1 != pos2 && pos1 != pos3){
                         if(pos2 != pos3){
-                            System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3]);
-                            System.out.println("" + letters[pos1] + letters[pos3] + letters[pos2]);
-                            System.out.println("" + letters[pos2] + letters[pos1] + letters[pos3]);
-                            System.out.println("" + letters[pos2] + letters[pos3] + letters[pos1]);
-                            System.out.println("" + letters[pos3] + letters[pos2] + letters[pos1]);
-                            System.out.println("" + letters[pos3] + letters[pos1] + letters[pos2]);
+                            searchForAword("" + letters[pos1] + letters[pos2] + letters[pos3], letterCombinationWord, allAvailableWordsBG, size);
+                            letterCombinationWord.setLength(0);
+
+//                            searchForAword("" + letters[pos1] + letters[pos3] + letters[pos2], letterCombinationWord, allAvailableWordsBG, size);
+//                            letterCombinationWord.setLength(0);
+//
+//                            searchForAword("" + letters[pos2] + letters[pos1] + letters[pos3], letterCombinationWord, allAvailableWordsBG, size);
+//                            letterCombinationWord.setLength(0);
+//
+//                            searchForAword("" + letters[pos2] + letters[pos3] + letters[pos1], letterCombinationWord, allAvailableWordsBG, size);
+//                            letterCombinationWord.setLength(0);
+//
+//                            searchForAword("" + letters[pos3] + letters[pos2] + letters[pos1], letterCombinationWord, allAvailableWordsBG, size);
+//                            letterCombinationWord.setLength(0);
+
+//                            searchForAword("" + letters[pos3] + letters[pos1] + letters[pos2], letterCombinationWord, allAvailableWordsBG, size);
+//                            letterCombinationWord.setLength(0);
+//                            System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3]);
+//                            System.out.println("" + letters[pos1] + letters[pos3] + letters[pos2]);
+//                            System.out.println("" + letters[pos2] + letters[pos1] + letters[pos3]);
+//                            System.out.println("" + letters[pos2] + letters[pos3] + letters[pos1]);
+//                            System.out.println("" + letters[pos3] + letters[pos2] + letters[pos1]);
+//                            System.out.println("" + letters[pos3] + letters[pos1] + letters[pos2]);
                         }
                     }
                 }
@@ -126,14 +179,22 @@ public class Main {
 
     }
 
-    private static void gen4LettersWords(char[] letters){
+    private static void gen4LettersWords(char[] letters, StringBuilder letterCombinationWord, String[] allAvailableWordsBG, int size){
         for(int pos1 = 0; pos1 < letters.length; pos1++){
             for(int pos2 = 0; pos2 < letters.length; pos2++){
                 for(int pos3 = 0; pos3 < letters.length; pos3++){
                     for(int pos4 = 0; pos4 < letters.length; pos4++){
                         if(pos1 != pos2 && pos1 != pos3 && pos1 != pos4){
                             if(pos2 != pos3 && pos2 != pos4){
-                                System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4]);
+
+                                searchForAword("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4], letterCombinationWord, allAvailableWordsBG, size);
+                                letterCombinationWord.setLength(0);
+
+
+//                                System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4]);
+
+
+
 //                                System.out.println("" + letters[pos1] + letters[pos2] + letters[pos4] + letters[pos3]);
 //                                System.out.println("" + letters[pos1] + letters[pos3] + letters[pos2] + letters[pos4]);
 //                                System.out.println("" + letters[pos1] + letters[pos3] + letters[pos4] + letters[pos2]);
@@ -147,7 +208,7 @@ public class Main {
         }
     }
 
-    private static void gen5LettersWords(char[] letters) {
+    private static void gen5LettersWords(char[] letters, StringBuilder letterCombinationWord, String[] allAvailableWordsBG, int size) {
         for(int pos1 = 0; pos1 < letters.length; pos1++){
             for(int pos2 = 0; pos2 < letters.length; pos2++){
                for(int pos3 = 0; pos3 < letters.length; pos3++){
@@ -158,8 +219,11 @@ public class Main {
 
                                      if(pos3 != pos4 && pos3 != pos5 && pos4 != pos5){
 
+                                         searchForAword("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4] + letters[pos5], letterCombinationWord, allAvailableWordsBG, size);
+                                         letterCombinationWord.setLength(0);
+
                                          //5 LETTERS
-                                         System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4] + letters[pos5]);
+//                                         System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4] + letters[pos5]);
 
                                          //3 LETTERS
 //                                           System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3]);
@@ -197,7 +261,7 @@ public class Main {
 //        }
     }
 
-    private static void gen6LettersWords(char[] letters) {
+    private static void gen6LettersWords(char[] letters, StringBuilder letterCombinationWord, String[] allAvailableWordsBG, int size) {
         for(int pos1 = 0; pos1 < letters.length; pos1++){
             for(int pos2 = 0; pos2 < letters.length; pos2++){
                 for(int pos3 = 0; pos3 < letters.length; pos3++){
@@ -210,9 +274,12 @@ public class Main {
                                         if(pos3 != pos4 && pos3 != pos5 && pos3 != pos6){
                                             if(pos4 != pos5 && pos4 != pos6){
                                                 if(pos5 != pos6){
-                                                    System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3] +
-                                                                            letters[pos4] + letters[pos5] + letters[pos6]
-                                                    );
+
+                                                    searchForAword("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4] + letters[pos5] + letters[pos6], letterCombinationWord, allAvailableWordsBG, size);
+                                                    letterCombinationWord.setLength(0);
+//                                                    System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3] +
+//                                                                            letters[pos4] + letters[pos5] + letters[pos6]
+//                                                    );
                                                 }
                                             }
                                         }
@@ -230,7 +297,7 @@ public class Main {
         }
     }
 
-    private static void gen7LettersWords(char[] letters){
+    private static void gen7LettersWords(char[] letters, StringBuilder letterCombinationWord, String[] allAvailableWordsBG, int size){
         for(int pos1 = 0; pos1 < letters.length; pos1++){
             for(int pos2 = 0; pos2 < letters.length; pos2++){
                 for(int pos3 = 0; pos3 < letters.length; pos3++){
@@ -244,10 +311,13 @@ public class Main {
                                                 if(pos4 != pos5 && pos4 != pos6 && pos4 != pos7){
                                                     if(pos5 != pos6 && pos5 != pos7){
                                                         if(pos6 != pos7){
-                                                            System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3]
-                                                                                  + letters[pos4] + letters[pos5] + letters[pos6]
-                                                                                  + letters[pos7]
-                                                            );
+
+                                                            searchForAword("" + letters[pos1] + letters[pos2] + letters[pos3] + letters[pos4] + letters[pos5] + letters[pos6] + letters[pos7], letterCombinationWord, allAvailableWordsBG, size);
+                                                            letterCombinationWord.setLength(0);
+//                                                            System.out.println("" + letters[pos1] + letters[pos2] + letters[pos3]
+//                                                                                  + letters[pos4] + letters[pos5] + letters[pos6]
+//                                                                                  + letters[pos7]
+//                                                            );
                                                         }
                                                     }
                                                 }
